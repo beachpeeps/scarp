c5f2d881dde96dda54c60fa4aad7e9adf7cc3687
Julia Fiedler <jfiedler@ucsd.edu>
2020-01-30 21:02:56 -0800

    interpolating to a 5 Hz grid
    needs work, but now both are on the same time grid for gif plotting

--- mfiles/plot_truckanddrone.m
+++ mfiles/plot_truckanddrone.m
@@ -5,11 +5,11 @@ figureDir = '../viz/';
 figureName = 'truckanddrone';
 
 %% take a look at the length of time of each collect
-figure
-plot(drone.Processed.t,zeros(size(drone.Processed.t)))
-hold on
-plot(truck.Processed.t,ones(size(truck.Processed.t)))
-ylim([-1 2])
+% figure
+% plot(drone.Processed.t,zeros(size(drone.Processed.t)))
+% hold on
+% plot(truck.Processed.t,ones(size(truck.Processed.t)))
+% ylim([-1 2])
 %% 
 % things are still in datenum here, watch out. Change:
 secondsinday = 24*60*60;
@@ -30,23 +30,74 @@ fs_drone = 1/dt_drone;
 clf
 tTruck = datetime(truck.Processed.t,'ConvertFrom','datenum');
 tDrone = datetime(drone.Processed.t,'ConvertFrom','datenum');
+% %%
+% figwidth = 6;
+% figheight = 3;
+% nrow = 1;
+% ncol = 1;
+% 
+% [hFig, ax] = makeFig(figwidth,figheight,nrow,ncol,'units','inches');
+% P3 = -37.2271;
+% xloc = max(find(drone.Processed.x<-37.2271));
+% 
+% plot(tTruck+seconds(18),truck.Processed.Zinterp2(:,xloc),'linewidth',2)
+% hold on
+% plot(tDrone,drone.Processed.Zinterp2(:,xloc),'linewidth',2)
+% ylabel('z NAVD88, m')
+% title('Water Level at P3')
+% legend('truck','drone')
+% 
+% ax(1).XLim = [datetime(2019,12,9,17,54,0) datetime(2019,12,9,17,58,0)];
+% ax(1).Position(2) = 0.15;
+% 
+% hFig.Color = 'none';
+% hFig.Renderer = 'painters';
+% print(hFig, [figureDir figureName '.png'],'-r300');
+
+%% interpolate on to 5 Hz grid
+
+% get 5 Hz in datenum
+dt = 1/(secondsinday*5);
+
+tstart = datetime(2019,12,9,17,42,0);
+
+tarray = datenum(tstart):dt:datenum(datetime(2019,12,9,17,46,0));
 %%
-figwidth = 6;
-figheight = 3;
-nrow = 1;
-ncol = 1;
+droneZ = nan(length(tarray),441);
 
-[hFig, ax] = makeFig(figwidth,figheight,nrow,ncol,'units','inches');
-xloc = 335;
-plot(tTruck+seconds(18),truck.Processed.Zinterp2(:,xloc),'linewidth',2)
-hold on
-plot(tDrone,drone.Processed.Zinterp2(:,xloc),'linewidth',2)
-ylabel('z NAVD88, m')
-title(['Water Level, x = ' num2str(drone.Processed.x(xloc)) ' m'])
-legend('truck','drone')
+for xi = 1:441
+    Z = drone.Processed.Zinterp2(:,xi);
+    if length(Z(~isnan(Z)))>100
+    droneZ(:,xi) = interp1(drone.Processed.t(~isnan(Z)),Z(~isnan(Z)),tarray,'nearest');
+    end
+end
+%%
+truckZ = nan(length(tarray),441);
 
-ax(1).XLim = [datetime(2019,12,9,17,54,0) datetime(2019,12,9,17,58,0)];
-ax(1).Position(2) = 0.15;
+for xi = 1:441
+    Z = truck.Processed.Zinterp2(:,xi);
+    if length(Z(~isnan(Z)))>100
+    truckZ(:,xi) = interp1(truck.Processed.t(~isnan(Z)),Z(~isnan(Z)),tarray,'nearest');
+    end
+end
 
-print(hFig, '-djpeg', [figureDir figureName '.jpeg'],'-r300');
 
+
+%%
+x = drone.Processed.x;
+cmap = lines(2);
+% i = 500
+for i=500:1200
+    h(1) = plot(x,droneZ(i,:),'.','color',cmap(2,:),'linewidth',2)
+    hold on
+    h(2) = plot(x,truckZ(i-18*5,:),'.','color',cmap(1,:),'linewidth',2)
+    pause(0.1)
+    ylim([0.5 4])
+    ylabel('z (m, navd88)')
+    xlabel('x (m)')
+    if i<1200
+    delete(h(1))
+    delete(h(2))
+    end
+    
+end
