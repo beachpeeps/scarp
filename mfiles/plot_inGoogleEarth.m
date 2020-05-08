b86f1f947f76057ad6a12fd446cb81b45c759dbe
Julia Fiedler <jfiedler@ucsd.edu>
2020-05-08 16:10:43 -0700

    nixing the 'boundary' method for the kml file - googleEarth hates it

--- mfiles/plot_inGoogleEarth.m
+++ mfiles/plot_inGoogleEarth.m
@@ -18,23 +18,33 @@ system([cmd fullfilename])
 
 %%
 
-truck = load('../mat/lidar/truck/RiProcess_NoRiPrecision - Scanner VZ-2000 Horizontal - 191209_174131_Scanner_VZ-2000_Horizontal_0 - originalpoints, (last).mat');
-k = boundary(truck.xyzti.x(1:100:end),truck.xyzti.y(1:100:end));
-
-
-[lat,lon] = utm2ll(truck.xyzti.x(k),truck.xyzti.y(k),11);
-kmlwritepolygon('truck.kml',lat,lon,'EdgeColor',lines(1),'LineWidth',3)
+% truck = load('../mat/lidar/truck/RiProcess_NoRiPrecision - Scanner VZ-2000 Horizontal - 191209_174131_Scanner_VZ-2000_Horizontal_0 - originalpoints, (last).mat');
+truckdir = '../mat/lidar/truck/';
+truckfile = dir([truckdir 'WaveScanDrone*']);
+truck = load([truckdir truckfile(4).name]);
+% WaveScanDrone_POINTCLOUDS_20200224_185757_Scanner_VZ-2000_Horizontal.mat');
+% truck = load('../mat/lidar/truck/WaveScanDrone_POINTCLOUDS_20200224_174302_Scanner_VZ-2000_Horizontal.mat');
+% k = boundary(truck.xyzti.x(1:100:end),truck.xyzti.y(1:100:end),0.8);
+cmap = lines(3);
+[lat,lon] = utm2ll(truck.xyzti.x(1:1000:end),truck.xyzti.y(1:1000:end),11);
+lats = [max(lat) min(lat)];
+lons = [max(lon) min(lon) ];
+kmlwritepolygon('truck.kml',lats,lons,'EdgeColor',cmap(3,:),'LineWidth',3)
 
 %%
 system([cmd fullfile(pwd,'truck.kml')])
 
 %%
-drone = load('../mat/lidar/drone/20191209_00582_TorreyRunup_H3.mat');
-k = boundary(drone.xyzti.x(1:100:end),drone.xyzti.y(1:100:end));
+% drone = load('../mat/lidar/drone/20191209_00582_TorreyRunup_H3.mat');
+drone = load('../mat/lidar/drone/20200224_00582_TorreyRunup_H5.mat');
 
-cmap = lines(2);
-[lat,lon] = utm2ll(drone.xyzti.x(k),drone.xyzti.y(k),11);
-kmlwritepolygon('drone.kml',lat,lon,'EdgeColor',cmap(2,:),'LineWidth',3)
+cmap = lines(3);
+% k = boundary(drone.xyzti.x(1:100:end),drone.xyzti.y(1:100:end));
+[lat,lon] = utm2ll(drone.xyzti.x(1:1000:end),drone.xyzti.y(1:1000:end),11);
+lats = [max(lat) min(lat)];
+lons = [max(lon) min(lon) ];
+kmlwritepolygon('drone.kml',lats,lons,'EdgeColor',cmap(2,:),'LineWidth',3)
 
 %%
 system([cmd fullfile(pwd,'drone.kml')])
+system([cmd fullfile(pwd,'sensors.kml')])
